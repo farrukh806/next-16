25aea4024ab22182da75feb037c5b1b46b361fe4
farrukh806 <farrukhjamal806@gmail.com>
2025-11-16 11:35:48 +0530

    Add new SVG icons and TypeScript configuration
    - Added clock.svg, file.svg, globe.svg, logo.png, mode.svg, pin.svg, and window.svg icons to the public/icons directory.
    - Introduced a new tsconfig.json file for TypeScript configuration with strict settings and support for React JSX.

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  console.log("Next.js 16 with Turbopack!");
+  return (
+   <main>
+      <h1>Welcome to Next.js 16!</h1>
+   </main>
+  );
+}
